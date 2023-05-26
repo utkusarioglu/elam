271f13ee62e3949130768317df0f94c1a55f7e0d
Utku Sarioglu <utkusarioglu@hotmail.com>
2023-05-26 10:39:39 +0000

    REPO SUBCOMMAND

--- elam.sh
+++ elam.sh
@@ -1,16 +1,29 @@
 #!/bin/bash
 
+source ${0%/*}/repo.sh
+
 # Elam entrypoint
 #
 # For now, this file only calls the script files. 
 # In time it should assume the role of the sole executable in the repo, 
 # with all other script files only offering functionality
 
+echo $1
+
 case "$1" in
-  update-status)
-    echo 'Update status'
-    ${0%/*}/update-status.sh
-    ;;
+  # update-status)
+  #   echo 'Update status'
+  #   ${0%/*}/update-status.sh
+  #   ;;
+
+  # init-repo)
+  #   echo 'Init repo'
+  #   ${0%/*}/init-repo.sh
+  #   ;;
+  
+  repo)
+    ${0%/*}/repo.sh "$@"
+  ;;
 
   start-template-update)
     echo 'Start template update'
@@ -27,10 +40,6 @@ case "$1" in
     ${0%/*}/finalize-template-update.sh
     ;;
   
-  init-repo)
-    echo 'Init repo'
-    ${0%/*}/init-repo.sh
-    ;;
   
   *)
     echo 'help'
---
+++ repo.sh
@@ -0,0 +1,16 @@
+function repo_subcommand {
+  case "$2" in
+    init)
+      ${0%/*}/init-repo.sh
+    ;;
+    
+    status)
+      ${0%/*}/update-status.sh
+    ;; 
+
+    *)
+       echo 'elam repo'
+       echo 'elam repo help is here'
+       ;;
+  esac
+}
