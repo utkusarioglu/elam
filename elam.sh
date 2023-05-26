6c8a562c4ba9b50c5b11d0d6e8b9dd7b46efd1f5
Utku Sarioglu <utkusarioglu@hotmail.com>
2023-05-26 10:29:04 +0000

    ELAM.SH FIRST ITERATION

---
+++ elam.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Elam entrypoint
+#
+# For now, this file only calls the script files. 
+# In time it should assume the role of the sole executable in the repo, 
+# with all other script files only offering functionality
+
+case "$1" in
+  update-status)
+    echo 'Update status'
+    ${0%/*}/update-status.sh
+    ;;
+
+  start-template-update)
+    echo 'Start template update'
+    ${0%/*}/start-template-update.sh
+    ;;
+  
+  reset-template-update)
+    echo 'Reset template update'
+    ${0%/*}/reset-template-update.sh
+    ;;
+  
+  finalize-template-update)
+    echo 'Finalize template update'
+    ${0%/*}/finalize-template-update.sh
+    ;;
+  
+  init-repo)
+    echo 'Init repo'
+    ${0%/*}/init-repo.sh
+    ;;
+  
+  *)
+    echo 'help'
+    ;;
+esac
